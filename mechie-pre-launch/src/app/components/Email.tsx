a943e6de9fdb203eff74e48534c54b4978b844df
AhnafMasud10 <ahnafmasud11@gmail.com>
2024-08-18 21:03:07 +1000

    email input made responsive

---
+++ mechie-pre-launch/src/app/components/Email.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { MailIcon } from '@heroicons/react/solid'; // Import the Mail icon from Heroicons
+
+export default function EmailNotification() {
+  return (
+    
+    <div className="my-1 justify-center md:w-[540px] md:h-auto sm:w-[450px] w-[400px] h-14 sm:pl-4 pl-2 pr-2 py-1  rounded-3xl border border-[#3e1993]/10 flex items-center">
+      <div className="flex-grow flex items-center gap-4">
+        {/* Icon Container */}
+        <div className="md:h-12 md:w-12 sm:w-10 sm:h-10 h-10 w-10 bg-white rounded-xl border border-[#3f1993]/10 flex items-center justify-center">
+          <MailIcon className="md:w-6 md:h-6 w-5 h-5  text-[#6f41d2]" />
+        </div>
+        
+        {/* Input Placeholder */}
+        <input
+                type="email"
+                placeholder="Enter your email..."
+                className="md:w-10 sm:w-8 w-6 flex-grow bg-transparent text-[#13072e]/60 text-base font-medium font-['Inter'] leading-normal focus:outline-none"
+              />
+      </div>
+      {/* Button */}
+      <div className="md:px-6 md:py-4 px-3 py-3  bg-[#6f41d2] rounded-tr-xl rounded-br-xl shadow-inner border border-[#a576ff] flex items-center">
+        <button className="text-white text-base font-medium font-['Inter']">
+          Notify me
+        </button>
+      </div>
+    </div>
+  );
+}
--- mechie-pre-launch/src/app/page.tsx
+++ mechie-pre-launch/src/app/page.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import { LaunchSoon } from "./components/launch-soon";
 import logoLarge from "./Image/vector-logo.png"
 import logoSmall from "./Image/logo-small.png"
+import Email from "./components/Email";
 export default function Home() {
   return (
     <div className="">
       
-      <div className="md:ml-20 ml-2 my-2 ">
+      <div className="md:ml-20 ml-2 sm:ml-4 my-2 ">
         <div className="flex "> 
         
         <div className="w-full ">  
@@ -16,13 +17,18 @@ export default function Home() {
             Accelerate Your Mechanical Engineering  
             <span className="font-medium font-['Inter'] gradient-text"> Career to the Next Level</span>
           </div>
-          <div className="md:w-[460px] mr-12 sm:text-[20px] text-[15px] font-normal font-['Inter'] leading-relaxed"> 
+          <div className="mb-5 md:w-[460px] mr-12 sm:text-[20px] text-[15px] font-normal font-['Inter'] leading-relaxed"> 
           Join our waitlist to access the platform on a priority basis as soon as we launch!
           </div>
+          {/* <Email></Email> */}
         </div>
+        
 
 
-          <div className="flex sm:mt-[20px] w-full md:max-w-[33%] sm:max-w-[23%] max-w-[40%]">
+        
+
+
+          <div className="flex sm:mt-[20px] w-full md:max-w-[33%] sm:max-w-[15%] max-w-[33%]">
 
             <div className=" hidden md:block">
                 <Image
@@ -41,11 +47,15 @@ export default function Home() {
                     className="justify-end"
                   />
               </div>
+
           </div>
+          
 
 
         </div> 
-        
+        <div className="flex justify-center mt-5 sm:flex sm:justify-start">
+          <Email ></Email>
+        </div>
     </div> 
       
     </div>
