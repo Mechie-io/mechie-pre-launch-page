b28d0d68d062378b462e616ca5635031ad4aa539
AhnafMasud10 <ahnafmasud11@gmail.com>
2024-08-21 23:53:17 +1000

    final commit before working on backend

--- mechie-pre-launch/src/app/components/Header.tsx
+++ mechie-pre-launch/src/app/components/Header.tsx
@@ -1,43 +1,31 @@
-import React, {useState, useEffect} from "react";
-import { FunctionComponent } from 'react';
-
-import logo from "../Image/Group 2.svg"
-import companyName from "../Image/mechie.svg"
-import Image from "next/image"
-const Header:FunctionComponent = () => {
+import React, { FunctionComponent } from "react";
+import Image from "next/image";
+import logo from "../Image/Group 2.svg";
+import companyName from "../Image/mechie.svg";
+import line from "../Image/line.png"
+const Header: FunctionComponent = () => {
     return (
-        <nav className="w-full relative border-[#f9fafb] border-b-1 border-solid box-border h-20 overflow-hidden shrink-0 py-2 px-12  flex flex-row items-center justify-canter text-left text-sm text-[#09090b] font-roboto">
-            <div className="flex-1 flex flex-row items-start justify-between max-w-[1344px]">
-                <div className="self-stretch flex flex-row items-center justify-start gap-5">
-                    <div className="self-stretch flex flex-col items-start justify-center py-0 pl-0 pr-6 box-border min-w-[141.33px]">
-                        <div className="w-[109.5px] relative h-6">
-                            <Image src={logo} alt ="logo" className="absolute h-full w-[25.84%] top-[0%] right-[74.16%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full"/>
-                            <Image src={companyName} alt="company name" className="absolute h-[79.58%] w-[65.94%] top-[9.98%] right-[0.02%] bottom-[10.43%] left-[34.05%] max-w-full overflow-hidden max-h-full"/>
-
-                        </div>
-
-                    </div>
-                    
-                </div>
-
-                <div className="flex flex-row items-center justify-start gap-5">
-                    
-                    {/* <div className="h-11 px-5 py-2 bg-[#6f41d2] hover:bg-[#a576ff]  rounded-xl shadow-inner border border-[#a576ff] flex items-center justify-center">
-                        <div className="relative leading-[20px] font-medium text-white text-sm font-['Inter'] text-center">
-                            Join Waitlist
-                        </div>
-                        
-                        </div> */}
-
-                </div>
-
+        <nav className="mr-1 w-full relative border-[#f9fafb] border-b-1 border-solid box-border h-20 overflow-hidden shrink-0 py-2 md:px-12 px-6 flex items-center justify-start text-left text-sm text-[#09090b] font-roboto">
+            <div className="flex items-center ">
+                <Image src={logo} alt="logo" className="h-6 w-auto mx-3" />
+                <Image src={companyName} alt="company name" className="h-6 w-auto" />
             </div>
-
+            <div className="flex items-center mr-2 ">
+                <Image src={line} alt="line" className="h-7 w-auto mx-2" />
+            </div>
+            <div className="flex items-center mr-2">
+                <div className="text-sm font-normal text-purple-900 font-sans w-26" style={{ lineHeight: '1.1' }}>
+                    leetcode for mechanical <br /> engineers
+                </div>
+                {/* Uncomment the following block if you want to add a button */}
+                {/* <div className="h-11 px-5 py-2 bg-[#6f41d2] hover:bg-[#a576ff] rounded-xl shadow-inner border border-[#a576ff] flex items-center justify-center ml-5">
+                    <div className="relative leading-[20px] font-medium text-white text-sm font-['Inter'] text-center">
+                        Join Waitlist
+                    </div>
+                </div> */}
+            </div>
         </nav>
     );
 };
-        
-        
-    
 
-export default Header
+export default Header;
--- mechie-pre-launch/src/app/page.tsx
+++ mechie-pre-launch/src/app/page.tsx
@@ -10,16 +10,17 @@ export default function Home() {
   return (
     <div className="">
       
-      <div className="md:ml-20 ml-2 sm:ml-4 my-2 ">
+      <div className="md:ml-20 ml-4 sm:ml-4 my-2 ">
         <div className="flex "> 
         
-        <div className="w-full ">  
-          <LaunchSoon />
-          <div className="my-5 h-auto text-[#13072e] text-[50px] sm:text-[60px] sm:leading-[64px] md:text-[85px] md:leading-[87px] font-medium font-['Inter'] leading-[54px]">
+        <div className="w-full "> 
+          <div className="md:mt-5"><LaunchSoon /></div>
+          
+          <div className="my-5 md:my-10 h-auto text-[#13072e] text-[50px] sm:text-[60px] sm:leading-[64px] md:text-[85px] md:leading-[89px] font-medium font-['Inter'] leading-[54px]">
             Accelerate Your Mechanical Engineering  
             <span className="font-medium font-['Inter'] gradient-text"> Career to the Next Level</span>
           </div>
-          <div className=" md:w-[460px] mr-12 sm:text-[20px] text-[15px] font-normal font-['Inter'] leading-relaxed my-5"> 
+          <div className=" md:w-[460px] mr-12 sm:text-[20px] text-[15px] font-normal font-['Inter'] leading-relaxed my-5 md:my-10"> 
           Join our waitlist to access the platform on a priority basis as soon as we launch!
           </div>
           {/* <Email></Email> */}
@@ -55,7 +56,7 @@ export default function Home() {
 
 
         </div> 
-        <div className="flex justify-center sm:flex  mb-12 ">
+        <div className="flex justify-center lg:justify-start sm:flex  mb-12 ">
           <Email ></Email>
         </div>
         {/* <TestimonialCarousel></TestimonialCarousel> */}
